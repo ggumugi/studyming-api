55042cd24d6a010c7c6319bd25916dd02fac707b
ggumugi <phs970425@gmail.com>
2025-02-12 15:05:43 +0900

    1

---
+++ models/studygroup.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize')
+
+module.exports = class Studygroup extends Sequelize.Model {
+   static init(sequelize) {
+      return super.init(
+         {},
+         {
+            sequelize,
+            timestamps: true, //createAt, updateAt ..등 자동 생성
+            underscored: false,
+            modelName: '',
+            tableName: '',
+            paranoid: false, //deleteAt 사용 X
+            charset: 'utf8mb4',
+            collate: 'utf8mb4_general_ci',
+         }
+      )
+   }
+
+   static associate(db) {}
+}
