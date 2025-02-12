c02be1c4f92886fa3dc107cd7e95c6514d0913ac
000Lee <1003j6@gmail.com>
2025-02-12 15:07:54 +0900

    pushpushbaby

---
+++ models/grouptag.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize')
+
+module.exports = class GroupTag extends Sequelize.Model {
+   static init(sequelize) {
+      return super.init(
+         {},
+         {
+            sequelize,
+            timestamps: true,
+            underscored: false,
+            modelName: '',
+            tableName: '',
+            paranoid: false,
+            charset: 'utf8mb4',
+            collate: 'utf8mb4_general_ci',
+         }
+      )
+   }
+
+   static associate(db) {}
+}
