fe5f3f3b7dbae79a6ffe75f4eae74c3666ecdd2b
ggumugi <phs970425@gmail.com>
2025-02-27 17:42:26 +0900

    화상 채팅 난리

--- server/socketServer.js
+++ server/socketServer.js
@@ -1,63 +1,46 @@
-// socketServer.js
 const { Server } = require('socket.io')
+const socketHandlers = require('./socketHandlers')
 
-// 소켓 서버 설정 함수
-function setupSocketServer(server) {
+/**
+ * Socket.IO 서버를 설정하는 함수
+ * @param {Object} server - HTTP 서버 인스턴스
+ * @param {Function} sessionMiddleware - Express 세션 미들웨어
+ * @returns {Object} Socket.IO 서버 인스턴스
+ */
+function setupSocketServer(server, sessionMiddleware) {
    const io = new Server(server, {
       cors: {
          origin: process.env.FRONTEND_APP_URL || '*',
          methods: ['GET', 'POST'],
          credentials: true,
       },
+      pingTimeout: 60000,
    })
 
+   // 세션 미들웨어 적용
+   if (sessionMiddleware) {
+      io.use((socket, next) => {
+         sessionMiddleware(socket.request, {}, next)
+      })
+   }
+
+   // 연결 이벤트 처리
    io.on('connection', (socket) => {
-      console.log('사용자 연결:', socket.id)
+      console.log(`사용자 연결: ${socket.id}`)
 
-      // 방 입장
-      socket.on('join_room', (data) => {
-         const { roomId } = data
-         socket.join(roomId)
-         console.log(`사용자 ${socket.id}가 방 ${roomId}에 입장했습니다.`)
+      // 방 입장 이벤트
+      socket.on('join_room', (data) => socketHandlers.handleJoinRoom(socket, io, data))
 
-         // 방에 있는 다른 사용자들에게 새 사용자 입장 알림
-         socket.to(roomId).emit('user-joined', { userId: socket.id, roomId })
-      })
+      // 화면 공유 시작 이벤트
+      socket.on('screen_share_started', (data) => socketHandlers.handleScreenShareStarted(socket, io, data))
 
-      // 화면 공유 시작
-      socket.on('screen_share_started', (data) => {
-         const { roomId } = data
-         console.log(`사용자 ${socket.id}가 화면 공유를 시작했습니다.`)
-         socket.to(roomId).emit('screen_share_started', { userId: socket.id })
-      })
+      // WebRTC 시그널링 이벤트
+      socket.on('offer', (data) => socketHandlers.handleOffer(socket, io, data))
+      socket.on('answer', (data) => socketHandlers.handleAnswer(socket, io, data))
+      socket.on('candidate', (data) => socketHandlers.handleCandidate(socket, io, data))
 
-      // 오퍼 처리
-      socket.on('offer', (data) => {
-         const { sdp, roomId, to, from } = data
-         console.log(`오퍼: ${from || socket.id} -> ${to}`)
-         io.to(to).emit('offer', { sdp, roomId, from: from || socket.id })
-      })
-
-      // 앤서 처리
-      socket.on('answer', (data) => {
-         const { sdp, roomId, to, from } = data
-         console.log(`앤서: ${from || socket.id} -> ${to}`)
-         io.to(to).emit('answer', { sdp, roomId, from: from || socket.id })
-      })
-
-      // ICE Candidate 처리
-      socket.on('candidate', (data) => {
-         const { candidate, roomId, to, from } = data
-         console.log(`ICE Candidate: ${from || socket.id} -> ${to}`)
-         io.to(to).emit('candidate', { candidate, roomId, from: from || socket.id })
-      })
-
-      // 연결 해제
-      socket.on('disconnect', () => {
-         console.log('사용자 연결 해제:', socket.id)
-         // 방에 있는 다른 사용자들에게 사용자 퇴장 알림
-         io.emit('user-left', { userId: socket.id })
-      })
+      // 연결 해제 이벤트
+      socket.on('disconnect', () => socketHandlers.handleDisconnect(socket, io))
    })
 
    return io
