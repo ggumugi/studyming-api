5f3f1ec0af8ebf89dd782b491d8ab7ff07dc4d19
ggumugi <phs970425@gmail.com>
2025-03-06 13:10:14 +0900

    config 변경

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.set('port', process.env.PORT || 8002)
 
 // 시퀄라이즈를 사용한 DB 연결
 sequelize
-   .sync({ force: false })
+   .sync({ force: false, alter: false })
    .then(() => {
       console.log('데이터베이스 연결 성공')
    })
